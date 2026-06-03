f2c52e5ef8ca25a0fa5285925be01d205fba6fec
mamadefootball63-tech <mamadefootball63@gmail.com>
2026-06-03 12:41:02 +0330

    Enable PWA installation

--- app.js
+++ app.js
@@ -159,3 +159,14 @@ function render() {
 }
 
 render();
+  if ("serviceWorker" in navigator) {
+window.addEventListener("load", () => {
+navigator.serviceWorker.register("./service-worker.js")
+.then(() => {
+console.log("Service Worker Registered");
+})
+.catch(err => {
+console.log("SW Error:", err);
+});
+});
+  }
